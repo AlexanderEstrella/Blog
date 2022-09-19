77a1fba063afcf735c57f7934e0777d6c26463ef
AlexanderEstrella <alexanderestrella23@gmail.com>
2022-09-18 20:00:56 -0400

    add posts request to home.ejs

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const path = require("path");
+const fs = require("fs");
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent =
@@ -16,9 +17,12 @@ const app = express();
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+let posts = [];
+
 app.get("/", (req, res) => {
   res.render("home", {
     startingContent: homeStartingContent,
+    posts: posts,
   });
 });
 
@@ -39,9 +43,12 @@ app.get("/compose", (req, res) => {
 });
 
 app.post("/compose", (req, res) => {
-  let name = req.body.fname;
-  let tarea = req.body.areat;
-  console.log(name, tarea);
+  const post = {
+    title: req.body.fname,
+    post: req.body.areat,
+  };
+  posts.push(post);
+  res.redirect("/");
 });
 
 app.listen(3000, () => {
