cd8b070a64343d475f2bfc118ec0e9d229987a01
AlexanderEstrella <alexanderestrella23@gmail.com>
2022-09-21 19:52:36 -0400

    using lodash to adjust the searching parameters; that will it will allow users or myself to search for specific post

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const path = require("path");
-const fs = require("fs");
+const _ = require("lodash")
+
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent =
@@ -52,13 +53,13 @@ app.post("/compose", (req, res) => {
 });
 
 app.get("/posts/:any", (req, res) => {
-  let found = req.params.any;
-
-  if (found === posts[0].title) {
+  let ovar = _.lowerCase(req.params.any);
+  let Sparams  = _.lowerCase(posts[0].title)
+if (ovar === Sparams) {
     console.log("True")
   } else {
     console.log("False")
-}
+  };
 
 })
 
