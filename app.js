77deff0bb47fb143b54742cf63fb0e7c5208cc9d
AlexanderEstrella <alexanderestrella23@gmail.com>
2022-10-25 18:58:43 -0400

    rendering database objects to a post page

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ const ejs = require("ejs");
 const mongoose = require("mongoose");
 const path = require("path");
 const _ = require("lodash");
-const fs = require("fs");
-const { parentPort } = require("worker_threads");
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -16,16 +14,12 @@ const aboutContent =
 const contactContent =
   "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-const postsSchema = new mongoose.Schema({
-  title: {
-    type: String,
-  },
-  post: {
-    type: String,
-  },
+const postSchema = new mongoose.Schema({
+  title: String,
+  post: String,
 });
 
-const Post = mongoose.model("Post", postsSchema);
+const Post = mongoose.model("Post", postSchema);
 
 const app = express();
 // Sets up the Express app to handle data parsing
@@ -64,29 +58,22 @@ app.post("/compose", (req, res) => {
     title: req.body.fname,
     post: req.body.areat,
   });
-  Newpost.save(function (err, results) {
-    if (err) {
+  Newpost.save(function (err) {
+    if (!err) {
       res.redirect("/");
-    } else {
-      posts.push(results);
     }
   });
-  res.redirect("/");
 });
 
 // using lodash to allow uses to search using the browser bar with almost any specials and be able to render the posts.
-app.get("/posts/:any", (req, res) => {
-  const ovar = _.lowerCase(req.params.any);
-
-  posts.forEach(function (post) {
-    const Sparams = _.lowerCase(post.title);
-
-    if (ovar === Sparams) {
-      res.render("post", {
-        title: post.title,
-        content: post.post,
-      });
-    }
+app.get("/posts/:postid", (req, res) => {
+  const requestedPostId = req.params.postid;
+  console.log(requestedPostId);
+  Post.findOne({ _id: requestedPostId }, function (err, fpost) {
+    res.render("post", {
+      title: fpost.title,
+      content: fpost.post,
+    });
   });
 });
 
--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const app = require("./app");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
-//dotenv.config({ path: "./config.env" });
+dotenv.config({ path: "./config.env" });
 
-//const DB = process.env.DATABASE;
+const DB = process.env.DATABASE;
 
-/* mongoose
+mongoose
   .connect(DB, {
     useNewUrlParser: true,
     useCreateIndex: true,
@@ -16,47 +16,6 @@ const dotenv = require("dotenv");
   .then((con) => {
     console.log("DB CONNECTION SUCCESS");
   });
-<<<<<<< HEAD
-=======
-/* const postsSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: [true, "must input a tittle"],
-    unique: true,
-  },
-  posts: {
-    type: String,
-    required: [true, "A post is required"],
-  },
-//});
-
-//const personSchema = mongoose.Schema({
- // name: {
-  //  type: String,
-   // required: [true, "must input a name"],
- // },
- // favoritepost: postsSchema,
-//});
-
-//const Person = mongoose.model("Person", personSchema);
-
-//const Post = mongoose.model("Post", postsSchema);
-
-/* const testPost = new Post({
-  title: "Day 3",
-  posts: "today was a slightly better day!",
-}); */
-//const testpost = new Post({
- // name: "day 7",
- // posts: "new things",
-//});
-
-//Person.updateOne({ name: "alex" }, { favoritepost: testpost }, function (err) {
-  //if (err) {
-   // console.log("error" + err);
- // }
-//});
->>>>>>> b0e24da22dd1edc3c0622353b61331a6399fa210
 
 app.listen(process.env.PORT || 5000, function () {
   console.log("Server running at port 5000");
